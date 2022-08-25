22cb51e58d9bf37011509fd73f5575b880c73c1b
Rabee Aljaramani <rabeejermani@gmail.com>
2022-08-25 12:25:01 +0400

    edit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { useState } from 'react';
 import Celebration from './Celebration';
 import { list } from './data';
 import logo from './logo.svg';
+import element1 from './element1.svg';
+import element2 from './element2.svg';
 function App() {
-  const [winner, setWinner] = useState('??????');
+  const [winner, setWinner] = useState('');
   const [done, setDone] = useState(false);
+  const [raffiling, setRaffiling] = useState(false)
   const getEmployee = () => {
     var employee =
       list[Math.floor(Math.random() * list.length)].name.toString();
     return employee;
   };
   const main = () => {
+    setRaffiling(true)
     const intervalID = setInterval(() => {
       setWinner(getEmployee());
     }, 100);
@@ -24,24 +28,22 @@ function App() {
     <div className="App">
       <div className="container">
         <img className="logo" src={logo} alt="logo" />
-        {!done ? (
-          <h1 className="btn-shaffle" onClick={main}>
+        {!done ? (raffiling?'': <h1 className="btn-shaffle" onClick={main}>
             RAFFLE
           </h1>
+         
         ) : (
-          ''
+          <>
+          <h2 className="winner-is">WINNER IS</h2>
+          <h2 className="cong">Congratulations</h2>
+          <Celebration />
+          <img className='element1' src={element1} alt="element1"/>
+          <img className='element2' src={element2} alt="element1"/>
+        </>
         )}
 
-        <h2 className="winner-is">WINNER IS</h2>
+        
         <h2 className="winner">{winner.toString()}</h2>
-        {done ? (
-          <>
-            <h2 className="cong">Congratulations</h2>
-            <Celebration />
-          </>
-        ) : (
-          ''
-        )}
       </div>
     </div>
   );
